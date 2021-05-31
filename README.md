d55ae1184aec9fd439265f34cf52680d22067cc6
Ritu Agrawal <rituagrawal@Ritus-MacBook-Air.local>
2021-05-30 21:34:00 -0500

    Interactive username entry

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 * [what is synchronous and asynchronous function in javascript](https://www.outsystems.com/blog/posts/asynchronous-vs-synchronous-programming/)
 * [JavaScript Callback Functions](https://www.freecodecamp.org/news/javascript-callback-functions-what-are-callbacks-in-js-and-how-to-use-them/)
+* Callback hells
--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,13 @@ import * as _ from 'lodash';
 
 let svc = new GithubApiService();
 
-svc.getUserInfo('rituAgr', (user: User) =>{
-    svc.getRepoInfo('rituAgr', (repos: Repo[]) =>{
+if(process.argv.length<3){
+      console.log("Plz enter git repo user name");
+    }else
+    {
+    let userName = process.argv[2]
+    svc.getUserInfo(userName, (user: User) =>{
+    svc.getRepoInfo(userName, (repos: Repo[]) =>{
     let sortedRepos = _.sortBy(repos,[(repo:Repo) => repo.forkCount]);
     let reversedSortedRepos = _.reverse(sortedRepos)
     let top3Repos = _.take(reversedSortedRepos,3);
@@ -17,4 +22,5 @@ svc.getUserInfo('rituAgr', (user: User) =>{
 
  }
 );
+}
 
